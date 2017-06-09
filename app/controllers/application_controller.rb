6c3674194d5b04fbf59b18a779db61dce8b13e53
Aniela M Wolkonowski <anielamw@gmail.com>
2017-06-09 15:12:18 -0400

    trying to make code climate happy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-      keys: [:first_name, :last_name, :user_name]
-    )
+      keys: [:first_name, :last_name, :user_name])
   end
 end
