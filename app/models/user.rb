864f33ee3c93d17feef87997f3dd32f350df3132
Aniela M Wolkonowski <anielamw@gmail.com>
2017-06-09 12:06:54 -0400

    pass model tests for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :user_name, presence: true, uniqueness: true
+
 end
