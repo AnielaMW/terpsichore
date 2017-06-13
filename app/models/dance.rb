13427fcca985926b404ebc6f62eb19fb4d7d320b
Aniela M Wolkonowski <anielamw@gmail.com>
2017-06-13 10:49:28 -0400

    updated seed file

--- app/models/dance.rb
+++ app/models/dance.rb
@@ -5,9 +5,9 @@ class Dance < ApplicationRecord
   belongs_to :tempo, optional: true
   belongs_to :key, optional: true
   belongs_to :dance_level, optional: true
-  has_many :dance_comments
-  has_many :dance_directions
-  has_many :dance_moods
+  has_many :dance_comments, dependent: :destroy
+  has_many :dance_directions, dependent: :destroy
+  has_many :dance_moods, dependent: :destroy
   has_many :moods, through: :dance_moods
 
   validates :title, presence: true, uniqueness: true
