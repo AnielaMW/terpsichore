7a39f420b53859cb71fe5d1503eaba6c48eda0ef
Aniela M Wolkonowski <anielamw@gmail.com>
2017-06-20 17:36:25 -0400

    add flex boxes for all user forms

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,12 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   validates :user_id, presence: true
+
+  def ima
+    self.dancer || self.caller || self.musician || self.band || self.choreographer || self.composer
+  end
+
+  def summary
+    ima || self.about
+  end
 end
